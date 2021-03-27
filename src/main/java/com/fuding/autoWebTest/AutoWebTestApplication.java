ec18496218e0b342d121ffffa17936052b8c35ab
avboer <531781663@qq.com>
2021-03-28 00:28:10 +0800

    init

---
+++ src/main/java/com/fuding/autoWebTest/AutoWebTestApplication.java
@@ -0,0 +1,13 @@
+package com.fuding.autoWebTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWebTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoWebTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuding/autoWebTest/AutoWebTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuding.autoWebTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoWebTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
